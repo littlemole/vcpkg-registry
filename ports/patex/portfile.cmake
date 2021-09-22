3ded1b0696561095d432993087e3a455fcf1c13a
mike <mike@oha7.org>
2021-09-22 22:42:29 +0200

    bump version patex

--- ports/patex/portfile.cmake
+++ ports/patex/portfile.cmake
@@ -13,7 +13,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO littlemole/patex
-    REF 2db617456f4960571634a4dd646ec0df1650a795
+    REF e5b13efe0102705a3fe250e8986c8dcfb7ee3c16
     SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
     HEAD_REF main
 #    [PATCHES <patch1.patch> <patch2.patch>...]
