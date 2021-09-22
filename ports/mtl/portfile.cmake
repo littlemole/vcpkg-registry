022ea4bddce19fdf96c46042e1af03ebdacbdee6
mike <mike@oha7.org>
2021-09-23 01:57:31 +0200

    bump version mtl

--- ports/mtl/portfile.cmake
+++ ports/mtl/portfile.cmake
@@ -16,7 +16,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO littlemole/mtl
-    REF 940dd482a9ffbebe3df353a88f7381672b0bfe5f
+    REF eea2b564fdf8127bde1afe485af442efdd1a937c
     SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
     HEAD_REF main
 )
