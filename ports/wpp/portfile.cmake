d9c2d0deafb10c4083645867f01855840a1a7caa
littlemole <littlemole@oha7.org>
2023-10-16 21:27:19 +0200

    wpp

--- ports/wpp/portfile.cmake
+++ ports/wpp/portfile.cmake
@@ -10,21 +10,21 @@
 #   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
 #
 
-#vcpkg_from_github(
-#    OUT_SOURCE_PATH SOURCE_PATH
-#    REPO littlemole/metacpp
-#    REF 543042a571126f599c1ab2c02a64d02b8a94cd88
-#    SHA512 d9b78c74f7f67ca5d72bdc673f101eceb25f52fe17ff89a7b5c410f40802274a0ed80b103c9e5d6e8c95b36285d19fc36fe5bf0e05c858b53542173cc77632df
-#    HEAD_REF main
-#    [PATCHES <patch1.patch> <patch2.patch>...]
-#)
-
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL ssh://marilyn.oha7.org/srv/git/wpp
-    REF d8d928f49a0653f514bd5a97b55ee1cfabb0b98d
-    HEAD_REF win32
+    REPO littlemole/wpp
+    REF ddba5caac7ba7971f353ddfc7d17500178ff2af6
+    SHA512 f2f15e1529b582d84ad41d31e89f1befa7d3b0df5238c5b9848bce555ef7b737f39ac7e2a4054932fcfffa9f703c58adc7dc98c3ae234b8e7e646e89399b5a48
+    HEAD_REF main
+   # [PATCHES <patch1.patch> <patch2.patch>...]
 )
+
+#vcpkg_from_git(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    URL ssh://example.org/srv/git/wpp
+#    REF d8d928f49a0653f514bd5a97b55ee1cfabb0b98e
+#    HEAD_REF win32
+#)
 
 
 vcpkg_configure_cmake(
