30af4f69fa4bf0ad732e6c702d0356a918a3cdef
littlemole <littlemole@oha7.org>
2023-10-18 23:16:08 +0200

    wpp with features

--- ports/wpp/portfile.cmake
+++ ports/wpp/portfile.cmake
@@ -27,10 +27,17 @@ vcpkg_from_github(
 #)
 
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+      libevent WITH_LIBEVENT
+      test WITH_TEST
+)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DWITH_LIBEVENT=Off -DWITH_TEST=Off
+    OPTIONS  ${FEATURE_OPTIONS} # -DWITH_LIBEVENT=Off -DWITH_TEST=Off
     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
